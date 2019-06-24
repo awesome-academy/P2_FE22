ddcadccec3f612a5459ad642574a3932d1eeee26
huynh-dac-phu <jiinhuynh@gmail.com>
2019-06-24 13:31:09 +0700

    add comment, share, recently product

---
+++ src/components/Detail/recently.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import getRecentlyProduct from '../../utils/apiCaller';
+import ProductCard from '../ProductPage/product_card';
+import {connect} from 'react-redux';
+class Rencently extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            arr: []
+        }
+    }
+    convertArraytoQuery = (array) => {
+        return array.map(item => `id=${item}&`).join("");
+    }
+    componentDidMount(){
+        const query = this.convertArraytoQuery(this.props.recently);
+        const endpoint = `product?${query}`;
+        getRecentlyProduct(endpoint, 'GET', null)
+            .then(res => {
+                this.setState({
+                    arr: res.data
+                })
+            })
+    }
+    render(){
+        const {arr} = this.state;
+        return(
+            <div className="product-grid__content">
+                {arr.map((item, index) => {
+                    return <ProductCard
+                            key={index}
+                            data={item}/>
+                })}
+            </div>
+        )
+    }
+}
+const mapStatetoProps = (state) => {
+    return {
+        recently: state.cartReducer.recently
+    }
+}
+export default connect(mapStatetoProps, null)(Rencently)
